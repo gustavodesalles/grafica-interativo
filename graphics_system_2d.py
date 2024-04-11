5ef3f3fdec6b754ee50d36049e0bb879be91b999
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2024-04-11 19:21:36 -0300

    Clipping WIP

--- graphics_system_2d.py
+++ graphics_system_2d.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from obj_descriptor import OBJDescriptor
 from transformation_2d import Transformation2D
+from viewport import Viewport
 from window import Window
 
 
@@ -15,22 +16,22 @@ class GraphicsSystem2D:
 
         self.canvas = tk.Canvas(master, width=800, height=500, bg='white')
         self.canvas.pack(side=tk.LEFT)
+        canvas_width = self.canvas.winfo_reqwidth()
+        canvas_height = self.canvas.winfo_reqheight()
 
+        viewport_margin = 20
         # self.window = [-300, -200, 300, 200]  # Coordenadas da janela
-        self.window = Window(-300, -200, 300, 200)
-        self.viewport = [100, 100, 700, 400]  # Coordenadas da viewport
+        self.window = Window(0, 0, canvas_width, canvas_height)
+        self.viewport = Viewport(canvas_width // 4 + viewport_margin,
+                                 canvas_height // 4 + viewport_margin,
+                                 3 * canvas_width // 4 - viewport_margin,
+                                 3 * canvas_height // 4 - viewport_margin)  # Coordenadas da viewport
 
-        # Adicionar rótulos para viewport e window
-        # self.label_viewport = tk.Label(master, text="Viewport", font=('Helvetica', 14))
-        # self.label_viewport.place(x=650, y=10)
-        # self.label_window = tk.Label(master, text="Window", font=('Helvetica', 14))
-        # self.label_window.place(x=100, y=10)
+        # Desenhar uma moldura ao redor da viewport
+        self.viewport_frame = self.canvas.create_rectangle(self.viewport.coordinates(), outline='green')
 
-        # Desenhar a borda da viewport
-        # self.viewport_border = self.canvas.create_rectangle(*self.viewport, outline='red', dash=(5, 5))
-
-        # Desenhar a borda da janela
-        # self.window_border = self.canvas.create_rectangle(*self.window, outline='blue')
+        # Inicializar a técnica de clipagem atual
+        self.clipping_method = 'parametric'
 
         self.setup_object_list_interface()
         self.setup_pan_interface()
@@ -41,10 +42,11 @@ class GraphicsSystem2D:
         self.setup_rotation_interface()
         self.setup_export_object_interface()
         self.setup_import_object_interface()
+        self.setup_clipping_interface()
 
         self.angle_vup = 0  # Inicializa o ângulo de rotação de Vup como 0
 
-    def transform_to_viewport(self, x, y):
+    def rotate_align_vup(self, x, y):
         # Obter os ângulos de rotação
         theta = np.radians(self.angle_vup)
         cos_theta = np.cos(theta)
@@ -53,35 +55,21 @@ class GraphicsSystem2D:
         # Aplicar rotação para alinhar vup com o eixo Y
         x_rotated = x * cos_theta - y * sin_theta
         y_rotated = x * sin_theta + y * cos_theta
+        return x_rotated, y_rotated
 
+    def transform_to_viewport(self, x_rotated, y_rotated):
         # Normalizar coordenadas
-        xmin, ymin, xmax, ymax = self.window.xmin, self.window.ymin, self.window.xmax, self.window.ymax
-        xvmin, yvmin, xvmax, yvmax = self.viewport
+        xmin, ymin, xmax, ymax = self.window.coordinates()
+        xvmin, yvmin, xvmax, yvmax = self.viewport.coordinates()
 
         xv = ((x_rotated - xmin) / (xmax - xmin)) * (xvmax - xvmin) + xvmin
         yv = (1 - (y_rotated - ymin) / (ymax - ymin)) * (yvmax - yvmin) + yvmin
 
-        return xv, yv, x_rotated, y_rotated
+        return xv, yv
 
     def setup_object_list_interface(self):
         self.object_list_frame = tk.Frame(self.master)
         self.object_list_frame.pack(side=tk.RIGHT)
-        self.object_list_title = tk.Label(self.object_list_frame, text="Object List")
-        self.object_list_title.pack()
-        self.object_list_label = tk.Label(self.object_list_frame, text="")
-        self.object_list_label.pack()
-
-    def setup_remove_object_interface(self):
-        self.object_name_label = tk.Label(self.object_list_frame, text="Object Name:")
-        self.object_name_label.pack()
-        self.entry_object_name = tk.Entry(self.object_list_frame)
-        self.entry_object_name.pack()
-        self.button_remove_object = tk.Button(self.object_list_frame, text="Remove Object", command=self.remove_object)
-        self.button_remove_object.pack()
-
-    def setup_object_list_interface(self):
-        self.object_list_frame = tk.Frame(self.master)
-        self.object_list_frame.pack()
         self.object_list_title = tk.Label(self.object_list_frame, text="Object List")
         self.object_list_title.pack()
         self.object_list_label = tk.Label(self.object_list_frame, text="")
@@ -112,7 +100,7 @@ class GraphicsSystem2D:
 
         self.button_add_object = tk.Button(self.master, text="Add Object", command=self.add_object)
         self.button_add_object.pack(side=tk.TOP)
-        
+
     def setup_pan_interface(self):
         button_pan_left = tk.Button(self.master, text="Pan Left", command=self.pan_left)
         button_pan_left.pack(side=tk.TOP)
@@ -133,11 +121,15 @@ class GraphicsSystem2D:
 
         # self.entry_transformation = tk.Entry(self.master)
         self.entry_transformation = tk.StringVar()
-        self.r1 = tk.Radiobutton(self.master, text="Translation", value="translation", variable=self.entry_transformation)
+        self.r1 = tk.Radiobutton(self.master, text="Translation", value="translation",
+                                 variable=self.entry_transformation)
         self.r2 = tk.Radiobutton(self.master, text="Scaling", value="scaling", variable=self.entry_transformation)
-        self.r3 = tk.Radiobutton(self.master, text="Rotation around origin", value="rotation", variable=self.entry_transformation)
-        self.r4 = tk.Radiobutton(self.master, text="Rotation around object's center", value="center_rotation", variable=self.entry_transformation)
-        self.r5 = tk.Radiobutton(self.master, text="Rotation around arbitrary point", value="arbitrary_rotation", variable=self.entry_transformation)
+        self.r3 = tk.Radiobutton(self.master, text="Rotation around origin", value="rotation",
+                                 variable=self.entry_transformation)
+        self.r4 = tk.Radiobutton(self.master, text="Rotation around object's center", value="center_rotation",
+                                 variable=self.entry_transformation)
+        self.r5 = tk.Radiobutton(self.master, text="Rotation around arbitrary point", value="arbitrary_rotation",
+                                 variable=self.entry_transformation)
         self.r1.pack()
         self.r2.pack()
         self.r3.pack()
@@ -186,6 +178,128 @@ class GraphicsSystem2D:
 
         self.button_import_object = tk.Button(self.master, text="Import Obj", command=self.import_object)
         self.button_import_object.pack()
+
+    def setup_clipping_interface(self):
+        # Adicionar radio buttons para selecionar a técnica de clipagem
+        self.label_clipping_method = tk.Label(self.master, text="Clipping Method")
+        self.label_clipping_method.pack()
+
+        self.var_clipping_method = tk.StringVar()
+        self.var_clipping_method.set('parametric')
+
+        self.radio_parametric = tk.Radiobutton(self.master, text="Parametric Clipping",
+                                               variable=self.var_clipping_method, value='parametric',
+                                               command=self.change_clipping_method)
+        self.radio_parametric.pack()
+
+        self.radio_cohen_sutherland = tk.Radiobutton(self.master, text="Cohen-Sutherland Clipping",
+                                                     variable=self.var_clipping_method, value='cohen_sutherland',
+                                                     command=self.change_clipping_method)
+        self.radio_cohen_sutherland.pack()
+
+    def change_clipping_method(self):
+        self.clipping_method = self.var_clipping_method.get()
+
+    def clip_point(self, x, y):
+        # Verificar se o ponto está dentro da viewport
+        return self.viewport.xmin <= x <= self.viewport.xmax and self.viewport.ymin <= y <= self.viewport.ymax
+
+    def clip_line(self, x1, y1, x2, y2):
+        if self.clipping_method == 'parametric':
+            return self.clip_parametric(x1, y1, x2, y2)
+        elif self.clipping_method == 'cohen_sutherland':
+            return self.clip_cohen_sutherland(x1, y1, x2, y2)
+
+    def clip_parametric(self, x1, y1, x2, y2):
+        # Implementação do algoritmo de clipagem usando a equação paramétrica da reta
+        # Checagem por meio da equação paramétrica envolvendo os limites da janela e a própria linha
+        # Retorna as coordenadas clipadas (x1_clip, y1_clip, x2_clip, y2_clip)
+        xmin, ymin, xmax, ymax = self.window.coordinates()
+        t1 = 0
+        t2 = 1
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        # Parâmetros da equação paramétrica da reta
+        p = [-dx, dx, -dy, dy]
+        q = [x1 - xmin, xmax - x1, y1 - ymin, ymax - y1]
+
+        for i in range(4):
+            if p[i] == 0:
+                if q[i] < 0:
+                    return None  # Linha está fora da janela
+            else:
+                r = q[i] / p[i]
+                if p[i] < 0:
+                    t1 = max(t1, r)
+                else:
+                    t2 = min(t2, r)
+
+        if t1 > t2:
+            return None  # Linha está fora da janela
+
+        x1_clip = x1 + t1 * dx
+        y1_clip = y1 + t1 * dy
+        x2_clip = x1 + t2 * dx
+        y2_clip = y1 + t2 * dy
+
+        return x1_clip, y1_clip, x2_clip, y2_clip
+
+    def clip_cohen_sutherland(self, x1, y1, x2, y2):
+        # Implementação do algoritmo de recorte de Cohen-Sutherland
+        # Retorna as coordenadas clipadas (x1_clip, y1_clip, x2_clip, y2_clip)
+        xmin, ymin, xmax, ymax = self.window.coordinates()
+        # Códigos de região para os pontos inicial e final da linha
+        code1 = self.calculate_region_code(x1, y1)
+        code2 = self.calculate_region_code(x2, y2)
+
+        while True:
+            if not (code1 | code2):  # Ambos pontos estão dentro da janela
+                return x1, y1, x2, y2
+
+            if code1 & code2:  # Ambos pontos estão fora da janela
+                return None
+
+            # Escolher um dos pontos fora da janela
+            code_outside = code1 if code1 else code2
+
+            # Encontrar o ponto de interseção
+            if code_outside & 1:  # Topo da janela
+                x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1)
+                y = ymax
+            elif code_outside & 2:  # Fundo da janela
+                x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1)
+                y = ymin
+            elif code_outside & 4:  # Direita da janela
+                y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1)
+                x = xmax
+            elif code_outside & 8:  # Esquerda da janela
+                y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1)
+                x = xmin
+
+            # Substituir o ponto fora da janela pelo ponto de interseção
+            if code_outside == code1:
+                x1, y1 = x, y
+                code1 = self.calculate_region_code(x1, y1)
+            else:
+                x2, y2 = x, y
+                code2 = self.calculate_region_code(x2, y2)
+
+    def calculate_region_code(self, x, y):
+        code = 0
+        xmin, ymin, xmax, ymax = self.window.coordinates()
+
+        if x < xmin:  # Esquerda da janela
+            code |= 1
+        elif x > xmax:  # Direita da janela
+            code |= 2
+        if y < ymin:  # Topo da janela
+            code |= 4
+        elif y > ymax:  # Fundo da janela
+            code |= 8
+
+        return code
 
     def import_object(self):
         file_path = self.entry_import_obj_name.get()
@@ -272,31 +386,37 @@ class GraphicsSystem2D:
 
     def apply_transformation(self, obj, transformation_matrix):
         if obj.type == 'Point':
-            new_coordinates = np.dot(transformation_matrix, np.array([obj.coordinate_x, obj.coordinate_y, 1]))
-            obj.coordinate_x = new_coordinates[0]
-            obj.coordinate_y = new_coordinates[1]
+            new_coordinates = np.dot(transformation_matrix, np.array([obj.coordinate_x_scn, obj.coordinate_y_scn, 1]))
+            obj.coordinate_x_scn = new_coordinates[0]
+            obj.coordinate_y_scn = new_coordinates[1]
         elif obj.type == 'Line':
-            obj.start_point = np.dot(transformation_matrix, np.array([obj.start_point[0], obj.start_point[1], 1]))
-            obj.end_point = np.dot(transformation_matrix, np.array([obj.end_point[0], obj.end_point[1], 1]))
+            obj.start_point_scn = np.dot(transformation_matrix, np.array([obj.start_point_scn[0], obj.start_point_scn[1], 1]))
+            obj.end_point_scn = np.dot(transformation_matrix, np.array([obj.end_point_scn[0], obj.end_point_scn[1], 1]))
         elif obj.type == 'Wireframe':
-            for i in range(len(obj.point_list)):
-                point = obj.point_list[i]
+            for i in range(len(obj.point_list_scn)):
+                point = obj.point_list_scn[i]
                 point_vector = np.dot(transformation_matrix, np.array([point[0], point[1], 1]))
                 obj.point_list[i] = (point_vector[0], point_vector[1])
 
     def draw_object(self, obj):
         if obj.type == 'Point':
-            x, y, obj.coordinate_x_scn, obj.coordinate_y_scn = self.transform_to_viewport(obj.coordinate_x, obj.coordinate_y)
-            self.canvas.create_oval(x, y, x+2, y+2, fill=obj.color)
+            obj.coordinate_x_scn, obj.coordinate_y_scn = self.rotate_align_vup(obj.coordinate_x_scn, obj.coordinate_y_scn)
+            x, y = self.transform_to_viewport(obj.coordinate_x_scn, obj.coordinate_y_scn)
+            if self.clip_point(x, y):
+                self.canvas.create_oval(x, y, x + 2, y + 2, fill=obj.color)
         elif obj.type == 'Line':
-            x1, y1, obj.start_point_scn[0], obj.start_point_scn[1] = self.transform_to_viewport(obj.start_point[0], obj.start_point[1])
-            x2, y2, obj.end_point_scn[0], obj.end_point_scn[1] = self.transform_to_viewport(obj.end_point[0], obj.end_point[1])
+            obj.start_point_scn[0], obj.start_point_scn[1] = self.rotate_align_vup(obj.start_point_scn[0], obj.start_point_scn[1])
+            obj.end_point_scn[0], obj.end_point_scn[1] = self.rotate_align_vup(obj.end_point_scn[0], obj.end_point_scn[1])
+            x1_clip, y1_clip, x2_clip, y2_clip = self.clip_line(obj.start_point_scn[0], obj.start_point_scn[1], obj.end_point_scn[0], obj.end_point_scn[1])
+            x1, y1 = self.transform_to_viewport(x1_clip, y1_clip)
+            x2, y2 = self.transform_to_viewport(x2_clip, y2_clip)
             self.canvas.create_line(x1, y1, x2, y2, fill=obj.color)
         elif obj.type == 'Wireframe':
             transformed_coords = []
-            for i in range(len(obj.point_list)):
-                point = obj.point_list[i]
-                x, y, obj.point_list_scn[i][0], obj.point_list_scn[i][1] = self.transform_to_viewport(point[0], point[1])
+            for i in range(len(obj.point_list_scn)):
+                point = obj.point_list_scn[i]
+                obj.point_list_scn[i][0], obj.point_list_scn[i][1] = self.rotate_align_vup(obj.point_list_scn[i][0], obj.point_list_scn[i][1])
+                x, y = self.transform_to_viewport(point[0], point[1])
                 transformed_coords.append((x, y))
             self.draw_wireframe(transformed_coords, obj.color)
 
@@ -306,6 +426,22 @@ class GraphicsSystem2D:
             x1, y1 = coordinates[i]
             x2, y2 = coordinates[(i + 1) % len(coordinates)]
             self.canvas.create_line(x1, y1, x2, y2, fill=color)
+
+        # Preencher o interior do polígono manualmente
+        scanline_y = min(y for x, y in coordinates)
+        while scanline_y <= max(y for x, y in coordinates):
+            intersections = []
+            for i in range(len(coordinates)):
+                x1, y1 = coordinates[i]
+                x2, y2 = coordinates[(i + 1) % len(coordinates)]
+                if y1 != y2 and (y1 <= scanline_y <= y2 or y2 <= scanline_y <= y1):
+                    x_intersect = x1 + (scanline_y - y1) * (x2 - x1) / (y2 - y1)
+                    intersections.append(x_intersect)
+            intersections.sort()
+            for i in range(0, len(intersections), 2):
+                self.canvas.create_line(intersections[i], scanline_y, intersections[i + 1], scanline_y,
+                                        fill='black')
+            scanline_y += 1
 
     def rotate_vup(self):
         angle = float(self.entry_rotation.get())
@@ -321,11 +457,26 @@ class GraphicsSystem2D:
             self.angle_vup = 0  # Definir como 0 se a entrada não for válida
 
         # Redesenha as bordas
-        # self.viewport_border = self.canvas.create_rectangle(*self.viewport, outline='red', dash=(5, 5))
-        # self.window_border = self.canvas.create_rectangle(*self.window, outline='blue')
+        self.viewport_border = self.canvas.create_rectangle(self.viewport.coordinates(), outline='red', dash=(5, 5))
+        self.window_border = self.canvas.create_rectangle(self.window.coordinates(), outline='blue')
 
         for obj in self.display_file.objects.values():
             self.draw_object(obj)
+            # if obj.type == 'Line':
+            #     x1, y1 = obj.start_point[0], obj.start_point[1]
+            #     x2, y2 = obj.end_point[0], obj.end_point[1]
+            #     if self.clipping_method == 'parametric':
+            #         clipped_coords = self.clip_parametric(x1, y1, x2, y2)
+            #     elif self.clipping_method == 'cohen_sutherland':
+            #         clipped_coords = self.clip_cohen_sutherland(x1, y1, x2, y2)
+            #     else:
+            #         clipped_coords = (x1, y1, x2, y2)
+            #     if clipped_coords and isinstance(clipped_coords, tuple):  # Verifica se clipped_coords é uma tupla
+            #         obj.start_point = clipped_coords[:2]
+            #         obj.end_point = clipped_coords[2:]
+            #         self.draw_object(obj)
+            # else:
+            #     self.draw_object(obj)
 
         # Atualizar a lista de objetos
         object_names = "\n".join(self.display_file.objects.keys())
@@ -374,7 +525,7 @@ class GraphicsSystem2D:
         coordinates = coordinates_str.split(",")
         coordinates_head = coordinates.pop(0)
         coordinates = list(map(lambda x: int(x), coordinates))
-        coordinates = [(coordinates[i], coordinates[i+1]) for i in range(0, len(coordinates), 2)]
+        coordinates = [(coordinates[i], coordinates[i + 1]) for i in range(0, len(coordinates), 2)]
 
         if coordinates_head.upper() == "POINT":
             self.display_file.add_point(coordinates[0], object_color)
