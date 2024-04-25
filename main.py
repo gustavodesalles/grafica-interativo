24047c802adc5963d379c032da954b51c2769c3b
Gustavo Fukushima <gustavo.fukushima@sc.senai.br>
2024-04-24 21:49:47 -0300

    B-Spline (sort of, again)

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ display_file = DisplayFile2D()
 # display_file.add_line(((-50, -50), (50, 50)), 'red')
 # display_file.add_point((0, 0))
 # display_file.add_wireframe([(100, -100), (100, 100), (-100, 100), (-100, -100)], 'blue')
-# control_points = [(-50, -50), (100, 0), (100, 0), (100, 100), (-50, 50), (0, 100), (0, 100), (-100, 100)]
+control_points = [(-50, -50), (100, 0), (100, 0), (100, 100), (-50, 50), (0, 100), (0, 100), (-100, 100)]
 # control_points = [(20, 20), (50, 20), (100, 50), (200, 50)]
-# display_file.add_b_spline(control_points, 'red')
+display_file.add_b_spline(control_points, 'red')
 
 object_list = tk.Listbox(root)
 
